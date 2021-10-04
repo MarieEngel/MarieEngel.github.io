5af2fe635bfd39dab4d1f84438d8cecaa92a959d
Marie Engel <marie.engel@deliveryhero.com>
2021-10-04 11:34:35 +0200

    added while loop for option 1 and 2

--- cli/query.py
+++ cli/query.py
@@ -22,89 +22,91 @@ if name_input != None:
     print(
         f"Hello, {name_input}! \nWhat would you like to do? \n1. List items by warehouse \n2. Search an item and place an order \n3. Quit"
     )
+# while loop to come back to operation selection after operation 1 or 2
+operation_input = None  #  condition varaible needs to be defined before the loop
+while operation_input != 3:
+    operation_input = int(input("Please type the number of the operation: "))
 
-    # operation_input = None
-    # while operation_input != 3:
-operation_input = int(input("Please type the number of the operation: "))
+    # If they pick 1
+    # bullet list
+    if operation_input == 1:
+        print("Items in Warehouse 1:")
+        for item in warehouse1:
+            print(f"- {item}")
+        print()
+        print("Items in Warehouse 2:")
+        for item in warehouse2:
+            print(f"- {item}")
 
-# If they pick 1
-# bullet list
-if operation_input == 1:
-    print("Items in Warehouse 1:")
-    for item in warehouse1:
-        print(f"- {item}")
-    print()
-    print("Items in Warehouse 2:")
-    for item in warehouse2:
-        print(f"- {item}")
+    # Else, if they pick 2
+    elif operation_input == 2:
+        item_input = input("What is the name of the item?  ")
 
-# Else, if they pick 2
-elif operation_input == 2:
-    item_input = input("What is the name of the item?  ")
+        available_wh1 = 0
+        available_wh2 = 0
 
-    available_wh1 = 0
-    available_wh2 = 0
+        # count items in warehouse1:
+        for item in warehouse1:
+            if item == item_input:
+                available_wh1 += 1
 
-    # count items in warehouse1:
-    for item in warehouse1:
-        if item == item_input:
-            available_wh1 += 1
+        # count items in warehouse2:
+        for item in warehouse2:
+            if item == item_input:
+                available_wh2 += 1
 
-    # count items in warehouse2:
-    for item in warehouse2:
-        if item == item_input:
-            available_wh2 += 1
+        # sum of available items
+        available = available_wh1 + available_wh2
 
-    # sum of available items
-    available = available_wh1 + available_wh2
+        # give the location of the item or "Not in stock"
+        if available > 0:  # both warehouses
+            print(f"Amount available: {available}")
+            if available_wh1 > 0 and available_wh2 > 0:
+                print("Location: Both warehouses")
+                if available_wh1 > available_wh2:  # maximum
+                    print(
+                        f"Maximum availability: {max(available_wh1, available_wh2)} in Warehouse1"
+                    )
+                elif available_wh1 < available_wh2:
+                    print(
+                        f"Maximum availability: {max(available_wh1, available_wh2)} in Warehouse2"
+                    )
+                else:
+                    print(
+                        f"Both warehouses have the same amount of {item_input} in stock."
+                    )
 
-    # give the location of the item or "Not in stock"
-    if available > 0:  # both warehouses
-        print(f"Amount available: {available}")
-        if available_wh1 > 0 and available_wh2 > 0:
-            print("Location: Both warehouses")
-            if available_wh1 > available_wh2:  # maximum
-                print(
-                    f"Maximum availability: {max(available_wh1, available_wh2)} in Warehouse1"
-                )
-            elif available_wh1 < available_wh2:
-                print(
-                    f"Maximum availability: {max(available_wh1, available_wh2)} in Warehouse2"
-                )
+            elif available_wh1 > 0:  # warehouse1 or warehouse#
+                print("Location: Warehouse1")
             else:
-                print(f"Both warehouses have the same amount of {item_input} in stock.")
+                print("Location: Warehouse2")
 
-        elif available_wh1 > 0:  # warehouse1 or warehouse#
-            print("Location: Warehouse1")
+            # order decision:
+            order_decision = input("Would you like to place an order? (y/n): ")
+
+            if order_decision == "y":
+                order_amount = int(
+                    input(f"How many {item_input} would you like to order?  ")
+                )
+                if order_amount > available:
+                    order_amount_correction = input(
+                        f"Sorry, your order is exceeding our stock. Would you like to order {available} {item_input} (y/n)? "
+                    )
+                    if order_amount_correction == "y":
+                        print(f"Your order has been placed: {available} {item_input}")
+                else:
+                    print(f"Your order has been placed: {order_amount} {item_input}")
         else:
-            print("Location: Warehouse2")
+            print("Sorry, not in stock.")
 
-        # order decision:
-        order_decision = input("Would you like to place an order? (y/n): ")
-
-        if order_decision == "y":
-            order_amount = int(
-                input(f"How many {item_input} would you like to order?  ")
-            )
-            if order_amount > available:
-                order_amount_correction = input(
-                    f"Sorry, your order is exceeding our stock. Would you like to order {available} {item_input} (y/n)? "
-                )
-                if order_amount_correction == "y":
-                    print(f"Your order has been placed: {available} {item_input}")
-            else:
-                print(f"Your order has been placed: {order_amount} {item_input}")
+    # Else, if they pick 3
+    elif operation_input == 3:
+        pass  # to avoid having  "Thanks for your visit" displayed twice
     else:
-        print("Sorry, not in stock.")
+        print("Sorry, the operation entered is not valid.")
+        break  #  to get out of the loop
 
-# Else, if they pick 3
-elif operation_input == 3:
-    pass  # to avoid having  "Thanks for your visit" displayed twice
-else:
-    print("Sorry, the operation entered is not valid.")
-    # break
-
-    # Thank the user for the visit
+# Thank the user for the visit
 print()
 print(f"Thanks for your visit, {name_input}!")
 

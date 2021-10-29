3415b7c276201415eeeb869e06791ec4a985054f
Marie Engel <marie.engel@deliveryhero.com>
2021-10-29 20:39:34 +0200

    Remove commented code

--- cli/query.py
+++ cli/query.py
@@ -12,11 +12,6 @@ for item in warehouse1:
 from data import stock
 from collections import Counter
 
-# YOUR CODE STARTS HERE
-
-# Get the user name
-# Greet the user
-# Show the menu and ask to pick a choice
 
 name_input = input("What is your user name?  ")
 if name_input != None:
@@ -24,20 +19,11 @@ if name_input != None:
         f"Hello, {name_input}! \nWhat would you like to do? \n1. List items by warehouse \n2. Search an item and place an order \n3. Quit"
     )
 # while loop to come back to operation selection after operation 1 or 2
-operation_input = None  #  condition varaible needs to be defined before the loop
+operation_input = None  #  condition variable needs to be defined before the loop
 while operation_input != 3:
     operation_input = int(input("Please type the number of the operation: "))
 
     # If they pick 1
-    # bullet list
-    # if operation_input == 1:
-    #     print("Items in Warehouse 1:")
-    #     for item in warehouse1:
-    #         print(f"- {item}")
-    #     print()
-    #     print("Items in Warehouse 2:")
-    #     for item in warehouse2:
-    #         print(f"- {item}")
     if operation_input == 1:
         result_w1 = []
         result_w2 = []
@@ -74,24 +60,6 @@ while operation_input != 3:
 
         sum_available = available_w1 + available_w2
 
-        # old code:
-        #         available_wh1 = 0
-        #         available_wh2 = 0
-
-        #         # count items in warehouse1:
-        #         for item in warehouse1:
-        #             if item == item_input:
-        #                 available_wh1 += 1
-
-        #         # count items in warehouse2:
-        #         for item in warehouse2:
-        #             if item == item_input:
-        #                 available_wh2 += 1
-
-        #         # sum of available items
-        #         available = available_wh1 + available_wh2
-
-        #         # give the location of the item or "Not in stock"
         if sum_available > 0:  # both warehouses
             print(f"Amount available: {sum_available}")
             if available_w1 > 0 and available_w2 > 0:
@@ -109,7 +77,7 @@ while operation_input != 3:
                         f"Both warehouses have the same amount of {item_input} in stock."
                     )
 
-            elif available_w1 > 0:  # warehouse1 or warehouse#
+            elif available_w1 > 0:  # warehouse1 or warehouse2
                 print("Location: Warehouse1")
             else:
                 print("Location: Warehouse2")
@@ -123,7 +91,7 @@ while operation_input != 3:
                 )
                 if order_amount > sum_available:
                     order_amount_correction = input(
-                        f"Sorry, your order is exceeding our stock. Would you like to order {available} {item_input} (y/n)? "
+                        f"Sorry, your order is exceeding our stock. Would you like to order {item_input} (y/n)? "
                     )
                     if order_amount_correction == "y":
                         print(
@@ -144,25 +112,3 @@ while operation_input != 3:
 # Thank the user for the visit
 print()
 print(f"Thanks for your visit, {name_input}!")
-
-
-# # print(len(warehouse1))
-# # print(warehouse1[-1])
-# # print(len(warehouse2))
-# # print(warehouse1[0])
-# # print(warehouse1[0])
-# # print(type(warehouse1[0]))
-
-# # Greet the user
-
-# # Show the menu and ask to pick a choice
-
-# # If they pick 1
-# #
-# # Else, if they pick 2
-# #
-# # Else, if they pick 3
-# #
-# # Else
-
-# # Thank the user for the visit
